50cdb88808570853d4b77b1b244c993a5dedb884
boye <boye_1990@aliyun.com>
2021-01-05 16:16:39 +0800

    封装DropDown组件 优化

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownDom">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">你好 {{title}}</a>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="display: block" v-if="isOpen">
       <slot></slot>
@@ -8,7 +8,7 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -19,12 +19,30 @@ export default defineComponent({
   },
   setup () {
     const isOpen = ref(false)
+    const dropdownDom = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const handler = (e: MouseEvent) => {
+      if (dropdownDom.value) {
+        // contains 方法可以检察 传入的 Dom元素 是否属于 调用 contains方法 的Dom元素。
+        // 以此判断，当前点击对象是不是在 dropdown 组件范围内。
+        if (!dropdownDom.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    // 在挂载dropdown组件的时候监听click事件，判断是否是点击除dropdown组件以外的Dom节点，如果是，就将isOpen设置为false，如果不是，不做处理。
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownDom
     }
   }
 })
